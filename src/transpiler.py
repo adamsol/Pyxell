d3e46063a0be36308405aaf91c9c8ee7cb2fe991
Adam Sołtysik <adam27.sol@gmail.com>
2020-09-21 21:03:51 +0200

    Disallow passing arrays directly as variadic arguments

--- src/transpiler.py
+++ src/transpiler.py
@@ -406,6 +406,10 @@ class PyxellTranspiler:
                     self.throw(node, err.NoConversion(value.type, type))
                 return self.function(value, d)
 
+            # Variadic functions must not be converted to function variables.
+            if value.type.isFunc() and any(arg.variadic for arg in value.type.args):
+                self.throw(node, err.UnknownType())
+
             # This is the only place where containers are not covariant during type checking.
             if not can_cast(value.type, type, covariance=False):
                 self.throw(node, err.NoConversion(value.type, type))
@@ -1597,6 +1601,9 @@ class PyxellTranspiler:
                     if name in named_args:
                         if i < len(pos_args):
                             self.throw(node, err.RepeatedArgument(name))
+                        if func_arg.variadic:
+                            self.throw(node, err.UnexpectedArgument(name))
+
                         expr = named_args.pop(name)
 
                     elif func_arg.variadic:
