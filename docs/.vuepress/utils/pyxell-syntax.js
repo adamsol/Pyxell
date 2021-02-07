238b3b4a7a55b761d95932b8e416374ff834b15b
Adam Sołtysik <adam27.sol@gmail.com>
2021-02-07 19:35:07 +0100

    Fix the list of language keywords

--- docs/.vuepress/utils/pyxell-syntax.js
+++ docs/.vuepress/utils/pyxell-syntax.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    atom: /\b(?:true|false|null|this|super|default)\b/,
+    atom: /\b(?:false|null|super|this|true)\b/,
     keyword: /\b(?:abstract|and|as|break|by|class|constructor|continue|def|destructor|do|elif|else|extern|for|func|hiding|if|in|is|label|lambda|not|only|or|print|return|skip|super|until|use|while|yield)\b/,
     variable_name: /\b[a-z_][\w']*/,  // so that apostrophes in variable names are handled properly
     type_name: /\b[A-Z][\w']*/,
